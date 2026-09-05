4e0bcfe69396c18fabe01de80b7ed2937f30fa8d
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-05 21:50:14 +0400

    Complete UROBOROS core package
    Expose the core state, relation, evolution engine and UROBOROS
    runtime through a single package interface.

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+from .state import State
+from .relation import Relation
+from .engine import Engine
+from .uroboros import Uroboros
+
+**all** = [
+"State",
+"Relation",
+"Engine",
+"Uroboros",
+]
