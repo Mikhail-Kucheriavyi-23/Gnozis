3fac18e82a8df23e7d198887a72d8cb6c18e44a6
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-08 09:34:39 +0400

    core: export endogenous evolution operator

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,14 @@
 from .state import State
 from .relation import Relation
 from .engine import Engine
+from .evolution import evolutionary_transition, select_next_state
 from .uroboros import Uroboros
 
 __all__ = [
     "State",
     "Relation",
     "Engine",
+    "evolutionary_transition",
+    "select_next_state",
     "Uroboros",
 ]
