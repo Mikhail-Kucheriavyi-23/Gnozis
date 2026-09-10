511e3dc1b06341d4e4655d43323a15ea0091725a
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-10 21:28:39 +0400

    core: export fundamental Psi type

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,11 @@
-from .state import State
+from .state import Psi, State
 from .relation import Relation
 from .engine import Engine
 from .evolution import evolutionary_transition, select_next_state
 from .uroboros import Uroboros
 
 __all__ = [
+    "Psi",
     "State",
     "Relation",
     "Engine",
