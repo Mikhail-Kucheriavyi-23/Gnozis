a3596fa1e376884ca9402bb7d070277be1f13e02
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-13 04:39:35 +0400

    Expose canonical Psi engine and transition API

--- core/__init__.py
+++ core/__init__.py
@@ -1,14 +1,21 @@
-from .state import State
+from .state import Psi, State
 from .relation import Relation
 from .engine import Engine
-from .evolution import evolutionary_transition, select_next_state
+from .psi_engine import PsiEngine
+from .psi_transition import PsiTransition, make_psi_transition
+from .evolution import evolutionary_transition, evolutionary_psi_transition, select_next_state
 from .uroboros import Uroboros
 
 __all__ = [
+    "Psi",
     "State",
     "Relation",
     "Engine",
+    "PsiEngine",
+    "PsiTransition",
+    "make_psi_transition",
     "evolutionary_transition",
+    "evolutionary_psi_transition",
     "select_next_state",
     "Uroboros",
 ]
