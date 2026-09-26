d53e260921d7c9975266417ad3073d278f071243
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-26 19:59:10 +0400

    feat: expose runtime ExecutionInput contract

--- core/__init__.py
+++ core/__init__.py
@@ -3,6 +3,14 @@ from .relation import Relation
 from .engine import Engine
 from .psi_engine import PsiEngine
 from .psi_transition import PsiTransition, make_psi_transition
+from .execution_contract import (
+    ExecutionInput,
+    execution_input_from_psi,
+    execution_input_identity,
+    state_digest,
+    state_id,
+    verify_execution_input,
+)
 from .evolution import evolutionary_transition, evolutionary_psi_transition, select_next_state
 from .uroboros import Uroboros
 
@@ -14,6 +22,12 @@ __all__ = [
     "PsiEngine",
     "PsiTransition",
     "make_psi_transition",
+    "ExecutionInput",
+    "execution_input_from_psi",
+    "execution_input_identity",
+    "state_digest",
+    "state_id",
+    "verify_execution_input",
     "evolutionary_transition",
     "evolutionary_psi_transition",
     "select_next_state",
